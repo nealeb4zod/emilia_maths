69724557bba73345ee0b5484101cbdeb1640f9ea
Neale Johnston <neale@team-johnston.com>
2020-10-26 20:48:36 +0000

    Tidy up

--- maths.py
+++ maths.py
@@ -2,7 +2,7 @@ import cowsay
 
 from random import randint
 from sum import Sum
-from os import system, name
+from os import system
 
 
 def clear():
@@ -19,10 +19,6 @@ def subtract(number_1, number_2):
 
 def multiply(number_1, number_2):
     return number_1 * number_2
-
-
-def get_number(range_start, range_end):
-    return randint(range_start, range_end)
 
 
 def input_sum_choice():
@@ -54,7 +50,6 @@ def get_sum_type(sum_choice):
 def create_addition_subtraction(sum_type):
     number_1 = randint(1, 100)
     number_2 = randint(1, 100)
-
     if sum_type == 1:
         sum_answer = add(number_1, number_2)
     else:
@@ -64,49 +59,31 @@ def create_addition_subtraction(sum_type):
             sum_answer = subtract(number_2, number_1)
             sum = Sum(number_2, number_1, sum_type, sum_answer)
             return sum
-
     sum = Sum(number_1, number_2, sum_type, sum_answer)
-
     return sum
 
 
 def create_multiplication(sum_type):
     number_1 = randint(1, 10)
     number_2 = randint(1, 10)
-
     sum_answer = multiply(number_1, number_2)
-
     sum = Sum(number_1, number_2, sum_type, sum_answer)
-
     return sum
 
 
 def get_sum_list(sum_type):
     if sum_type == 4:
-        return get_list_of_random_sums()
+        random_list = [randint(1, 3) for iteration in range(10)]
+        return get_list_of_sums(random_list)
     else:
-        return get_list_of_same_sums(sum_type)
+        same_list = [sum_type for i in range(10)]
+        return get_list_of_sums(same_list)
 
 
-def get_list_of_random_sums():
+def get_list_of_sums(list_of_sum_types):
     list_of_sums = []
-    for i in range(10):
-        sum_type = randint(1, 3)
-        if sum_type == 1:
-            list_of_sums.append(create_addition_subtraction(sum_type))
-        elif sum_type == 2:
-            list_of_sums.append(create_addition_subtraction(sum_type))
-        elif sum_type == 3:
-            list_of_sums.append(create_multiplication(sum_type))
-    return list_of_sums
-
-
-def get_list_of_same_sums(sum_type):
-    list_of_sums = []
-    for i in range(10):
-        if sum_type == 1:
-            list_of_sums.append(create_addition_subtraction(sum_type))
-        elif sum_type == 2:
+    for sum_type in list_of_sum_types:
+        if sum_type == 1 or 2:
             list_of_sums.append(create_addition_subtraction(sum_type))
         elif sum_type == 3:
             list_of_sums.append(create_multiplication(sum_type))
@@ -120,11 +97,17 @@ def ask_questions(list_of_sums):
         maths_symbol = get_sum_type(sum.sum_type)
         formatted_sum = f"{sum.number_1} {maths_symbol} {sum.number_2} = ?"
         cowsay.cow(formatted_sum)
-        # print(formatted_sum)
-        inputted_user_answer = input("Please enter your answer: ")
-        sum.user_answer = inputted_user_answer
+        user_answer = input("Please enter your answer: ")
+        sum.user_answer = user_answer
         answered_questions.append(sum)
     return answered_questions
+
+
+def print_completed_sum_list(list_of_completed_sums):
+    for sum in list_of_completed_sums:
+        maths_symbol = get_sum_type(sum.sum_type)
+        formatted_sum = f"{sum.number_1} {maths_symbol} {sum.number_2} = {sum.sum_answer}  Your answer: {sum.user_answer}"
+        print(formatted_sum)
 
 
 def display_results(answered_questions):
@@ -138,27 +121,17 @@ def display_results(answered_questions):
             wrong_answers.append(sum)
     no_of_right_answers = len(right_answers)
     print(f"You got {no_of_right_answers} out of 10!\n")
-
     if len(right_answers) > 0:
         print("Right Answers:\n")
-        for r in right_answers:
-            maths_symbol = get_sum_type(r.sum_type)
-            formatted_sum = f"{r.number_1} {maths_symbol} {r.number_2} = {r.sum_answer}  Your answer: {r.user_answer}"
-            print(formatted_sum)
+        print_completed_sum_list(right_answers)
     if len(wrong_answers) > 0:
         print("\nWrong Answers:\n")
-        for w in wrong_answers:
-            maths_symbol = get_sum_type(w.sum_type)
-            formatted_sum = f"{w.number_1} {maths_symbol} {w.number_2} = {w.sum_answer}  Your answer: {w.user_answer}"
-            print(formatted_sum)
+        print_completed_sum_list(wrong_answers)
 
 
 def main():
-
     sum_choice = input_sum_choice()
-    # sum_type = get_sum_type(sum_choice)
     list_of_sums = get_sum_list(sum_choice)
-
     list_of_answers = ask_questions(list_of_sums)
     display_results(list_of_answers)
 
